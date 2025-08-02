044be9ff545babcc2344d7d6060c4357def685bd
AnsySan <epaskevic4@gmail.com>
2025-08-02 20:54:56 +0300

    first commit

--- authentication_service/src/main/java/com/ansysan/project/authentication_service/AuthenticationServiceApplication.java
+++ authentication_service/src/main/java/com/ansysan/project/authentication_service/AuthenticationServiceApplication.java
@@ -1,9 +1,20 @@
 package com.ansysan.project.authentication_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableFeignClients("com.ansysan.project.authentication_service.client")
+@EnableRetry
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User Service",
+                version = "1.0.0")
+)
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
