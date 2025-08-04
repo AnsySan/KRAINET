63509e1e3caeccda3f23d7d41da299e22d0e8e9b
AnsySan <epaskevic4@gmail.com>
2025-08-04 13:33:45 +0300

    exception handler

---
+++ notification_service/src/main/java/com/ansysan/project/notification_service/handler/EmailException.java
@@ -0,0 +1,7 @@
+package com.ansysan.project.notification_service.handler;
+
+public class EmailException extends RuntimeException {
+    public EmailException(String message) {
+        super(message);
+    }
+}
